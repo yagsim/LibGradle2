e47180e8ee15ba7eff6908b468a81e412970c6b1
yagsim <yagosimofernandez@gmail.com>
2022-02-11 09:33:48 +0100

    Subiendo Gradle

--- src/main/java/Gradle.java
+++ src/main/java/Gradle.java
@@ -10,10 +10,10 @@ import static java.rmi.server.LogStream.log;
 public class Gradle {
     public static void main(String[] args) throws IOException {
         Document doc =  Jsoup.connect("https://weather.com/es-ES/tiempo/hoy/l/9828ef4989632378cb01f05c18a0ca3a47781f777d705291ec60422ef13d871c/").get();
-        log(((org.jsoup.nodes.Document) doc).title());
-        Elements newsHeadlines1 = ((org.jsoup.nodes.Document) doc).select(".CurrentConditions--location--kytel");
-        Elements newsHeadlines2 = ((org.jsoup.nodes.Document) doc).select(".CurrentConditions--tempValue--3a50n");
-        Elements newsHeadlines3 = ((org.jsoup.nodes.Document) doc).select(".CurrentConditions--phraseValue--kytel");
+        log(doc.title());
+        Elements newsHeadlines1 = doc.select(".CurrentConditions--location--kytel");
+        Elements newsHeadlines2 =  doc.select(".CurrentConditions--tempValue--3a50n");
+        Elements newsHeadlines3 = doc.select(".CurrentConditions--phraseValue--kytel");
         for (Element headline : newsHeadlines1) {
             System.out.println(headline.html());
         }
